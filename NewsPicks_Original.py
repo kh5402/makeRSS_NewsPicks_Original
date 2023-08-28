8e6aaa7e642f91df4d332cc392e1b7466eb29ea9
kh <127534515+kh5402@users.noreply.github.com>
2023-08-28 14:01:59 +0900

    Update NewsPicks_Original.py

--- NewsPicks_Original.py
+++ NewsPicks_Original.py
@@ -40,52 +40,28 @@ def create_rss_feed():
         description="最新のNewsPicksのオリジナル記事をお届けします",
     )
 
-    # 最初の記事の情報を取得
-    first_article_div = soup.find('div', class_="news-card vertical big clear")
-    print(first_article_div)
+    # 複数の記事の情報を取得
+    article_divs = soup.find_all('div', class_="news-card vertical")
 
-    if first_article_div is None:
-        print("first_article_div が None やで！クラス名やタグが正しいか確認してみてな！")
-        # デバッグ情報として、対象のHTMLの一部を出力
-        print(soup.prettify()[:500]) # 最初の500文字を出力
-    else:
-        a_tag = first_article_div.find('a', href=True)
-        title_tag = first_article_div.find(class_="title _ellipsis")
-        subtitle_tag = first_article_div.find('span', class_="publisher _ellipsis")
-        time_tag =first_article_div.get('data-key')
-        
-    title = title_tag.text
-    subtitle = subtitle_tag.text
-    href = a_tag['href']
-    
-    date_str = time_tag[:14]  # 最初の14文字だけ取る
-    date = datetime.strptime(date_str, '%Y%m%d%H%M%S')
+    if not article_divs:
+        print("記事が見つからんかったわ！クラス名やタグが正しいか確認してみてな！")
+        return
 
-    feed.add_item(
-        title=title + " - " + subtitle,
-        link=href,
-        pubdate=date,
-        description="",
-    )
+    for article_div in article_divs:
+        a_tag = article_div.find('a', href=True)
+        title_tag = article_div.find(class_="title _ellipsis")
+        subtitle_tag = article_div.find('span', class_="publisher _ellipsis")
+        time_tag = article_div.get('data-key')
 
-    # 2個目以降の記事の情報を取得してRSSフィードに追加
-    for a_tag in soup.find_all('a', class_="css-dv7pnt", href=True):
-        if a_tag is None:
-            continue
-        title_tag = a_tag.find(class_="typography css-1ta5siq")
-        subtitle_tag = a_tag.find(class_="typography css-rvnxno")
-        time_tag = a_tag.find('time', datetime=True)
-        href = a_tag['href']
-
-        if title_tag and subtitle_tag and time_tag:
+        if a_tag and title_tag and subtitle_tag and time_tag:
             title = title_tag.text
             subtitle = subtitle_tag.text
-            date = parse(time_tag['datetime'])
-
-            full_title = title + " - " + subtitle
+            href = a_tag['href']
+            date_str = time_tag[:14]  # 最初の14文字だけ取る
+            date = datetime.strptime(date_str, '%Y%m%d%H%M%S')
 
             feed.add_item(
-                title=full_title,
+                title=title + " - " + subtitle,
                 link=href,
                 pubdate=date,
                 description="",
